f24e82dfde0e374f7ab694506114e8b353edeeee
Keys-digital <eseogheneikoyo23@gmail.com>
2025-02-16 11:35:11 -0500

    Enhance registration form handling: Implemented state management, integrated API call, processed JSON response, stored Bearer token, and added error handling.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,9 +3,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
-import DetailsPage from './components/DetailsPage/DetailsPage';import SearchPage from './components/SearchPage/SearchPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
